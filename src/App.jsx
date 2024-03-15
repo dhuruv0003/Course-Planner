d7383fd26eaefb87933b71b05a9485ae3660e2a5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-16 03:59:00 +0530

    "new commit"

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
     console.log(text);
   }
 
-// for useEffect Case 1 => Run on every Render
+  // for useEffect Case 1 => Run on every Render
   // jab bhi page render hoga useeffect wala hook chalega
 
   // useEffect(()=>{
   //   console.log("UI is rendering");
   // })
-    
-// for case 2=> First time render. This hook runs only on first time when the page is rendered. Here we use dependency array.
+
+  // for case 2=> First time render. This hook runs only on first time when the page is rendered. Here we use dependency array.
 
   // useEffect(()=>{
   //   console.log("render only once");
@@ -24,12 +24,21 @@ function App() {
 
   // case 3=> on every render on particular dependencies whenever change occur in them..
 
-    useEffect(()=>{
-      console.log("text is changed");
-    },[text])
+  // useEffect(()=>{
+  //   console.log("text is changed");
+  // },[text])
 
+  // case 4 => To handle unmounting of a component (Unmounting is when a React component is removed from the DOM).
 
+  useEffect(() => {
+    //add event listener
+    console.log("listender added");
 
+    //Note once the listener is added it first executes the return statement then add another listener.
+    return()=>{
+      console.log("listener removed");
+    }
+  },[text])
 
 
 
