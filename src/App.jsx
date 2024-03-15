1254bbc40a4a6bc56510ee6b8ad2ea2bf615b19f
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-16 02:51:41 +0530

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [text,settext]=useState("")
+  function changehandler(event){
+    settext(()=>event.target.value);
+    console.log(text);
+  }
+  return (
+    <div className='App'>
+        <input type="text" onChange={changehandler}/>
+    </div>
+  )
+}
+
+export default App
