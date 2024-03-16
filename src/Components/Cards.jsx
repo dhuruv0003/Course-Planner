de80a80dd4bbd03cdf3dc8a732cf83f154706c9a
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-17 03:41:38 +0530

    "new commit"

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -5,23 +5,23 @@ export default function Cards(courses) {
     //The data within the api courses is in key value pairs. where object is courses. and kays are (business,design development ,....) theses are coursse category. Now  but we want only values (array of elements of the keys/object
     //get courses is a funtion that retur an array of all the values in api
 
-    const allCourses = [];
+    let allCourses = [];
     const getCourses = () => {
         Object.values(courses).forEach((courseCategory) => {
-           courseCategory.forEach((course)=>{
-            allCourses.push(course)
-           })
+            courseCategory.forEach((course) => {
+                allCourses.push(course);
+            })
         })
         return allCourses;
     }
-// getCourses=[ {description : "This course provides an overview of marketing principles and practices, including market research, segmentation, targeting, and positioning. Students will learn how to create effective marketing plans and campaigns, using both traditional and digital marketing techniques.", id : "MK101" image : {url: 'https://codehelp-apis.vercel.app/get-top-courses/Business/Introduction%20To%20Marketing.png', alt: 'Introduction to Marketing'}  title : "Introduction to Marketing"},{},{},{}.......]
+    // getCourses=[ {description : "This course provides an overview of marketing principles and practices, including market research, segmentation, targeting, and positioning. Students will learn how to create effective marketing plans and campaigns, using both traditional and digital marketing techniques.", id : "MK101" image : {url: 'https://codehelp-apis.vercel.app/get-top-courses/Business/Introduction%20To%20Marketing.png', alt: 'Introduction to Marketing'}  title : "Introduction to Marketing"},{},{},{}.......]
 
     return (
         <div className="">
-            {getCourses().map((course)=>{
+            {getCourses().map((course) => {
                 <Card course={course}></Card>
             })}
         </div>
-       
+
     )
 }
