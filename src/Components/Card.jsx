7f87d1a44583712cc6016c580ec9e2cb53c50f0a
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-17 03:38:24 +0530

    "new commit"

---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import {FcLike} from "react-icons/fc"
+
+export default function Card({course}){
+    return <div>
+        <div className="">
+            <img src={course.image.url} alt="" />
+            <div className="">
+                <button>
+                    <FcLike fontSize="1.75rem"></FcLike>
+                </button>
+            </div>
+        </div>
+    </div>
+}
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Card from "./Card"
 export default function Cards(courses) {
 
     //The data within the api courses is in key value pairs. where object is courses. and kays are (business,design development ,....) theses are coursse category. Now  but we want only values (array of elements of the keys/object
@@ -15,6 +15,7 @@ export default function Cards(courses) {
         return allCourses;
     }
 // getCourses=[ {description : "This course provides an overview of marketing principles and practices, including market research, segmentation, targeting, and positioning. Students will learn how to create effective marketing plans and campaigns, using both traditional and digital marketing techniques.", id : "MK101" image : {url: 'https://codehelp-apis.vercel.app/get-top-courses/Business/Introduction%20To%20Marketing.png', alt: 'Introduction to Marketing'}  title : "Introduction to Marketing"},{},{},{}.......]
+
     return (
         <div className="">
             {getCourses().map((course)=>{
